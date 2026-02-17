b7dc3aa1c9b0a8b1a9f2eabc7e3ef406f79c11a0
saraalemanno <sara.alemanno12@gmail.com>
2026-02-17 14:33:27 +0100

    Modificato codice Route perchè sia eseguibile da desktop + aggiunta send_cfg_plc

--- Route_Tests_Bucintoro_v0.py
+++ Route_Tests_Bucintoro_v0.py
@@ -305,5 +305,5 @@ def download_log():
 
 # Run the Flask application
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
 
